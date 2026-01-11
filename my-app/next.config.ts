a886f0bd6966b1a8f439f3e6a2006fc49ab52e1e
b.bryxnnn <bryanong.2551@gmail.com>
2026-01-11 12:42:26 +0700

    fix: clean config and add SSR to admin page

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -1,16 +1,7 @@
 import type { NextConfig } from "next";
-import path from "path"; // 1. อย่าลืม import path
 
 const nextConfig: NextConfig = {
-  // 2. ใส่ config ส่วนนี้เพิ่มเข้าไปครับ
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      react: path.resolve(__dirname, 'node_modules/react'),
-      'react-dom': path.resolve(__dirname, 'node_modules/react-dom'),
-    };
-    return config;
-  },
+  /* config options here */
 };
 
 export default nextConfig;
